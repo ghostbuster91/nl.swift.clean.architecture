b0e04c3a3d44503b70587589f5fa9d93fa22ebbf
Ordon <ordon1313@gmail.com>
2016-03-17 10:18:31 +0100

    gateway field

--- app/src/main/java/nl/mdevcon/speakerslist/ShowAllSpeakersInteractor.java
+++ app/src/main/java/nl/mdevcon/speakerslist/ShowAllSpeakersInteractor.java
@@ -1,6 +1,13 @@
 package nl.mdevcon.speakerslist;
 
 public class ShowAllSpeakersInteractor {
+
+    final EntityGatewayProtocol gateway;
+
+    public ShowAllSpeakersInteractor(EntityGatewayProtocol gateway) {
+        this.gateway = gateway;
+    }
+
     void showAllSpeakers() {
 
     }
