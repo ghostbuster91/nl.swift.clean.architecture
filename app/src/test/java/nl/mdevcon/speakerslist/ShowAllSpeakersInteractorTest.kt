03c89c20e3d8dc85743b4f61f589d5a8276f44a7
Ordon <ordon1313@gmail.com>
2016-03-17 10:51:53 +0100

    convert test to kotlin

---
+++ app/src/test/java/nl/mdevcon/speakerslist/ShowAllSpeakersInteractorTest.kt
@@ -0,0 +1,13 @@
+package nl.mdevcon.speakerslist
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ShowAllSpeakersInteractorTest {
+
+    @Test
+    fun shouldDisplayAllSpeakersFromGateway() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
